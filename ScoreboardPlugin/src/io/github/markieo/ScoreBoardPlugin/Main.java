06593f332f26c89acb1f9151d07b691c1f213017
markieo <runescape-fun12@hotmail.com>
2013-09-24 19:37:31 +0200

    onenable en ondisable geadd

--- ScoreboardPlugin/src/io/github/markieo/ScoreBoardPlugin/Main.java
+++ ScoreboardPlugin/src/io/github/markieo/ScoreBoardPlugin/Main.java
@@ -1,5 +1,12 @@
 package io.github.markieo.ScoreBoardPlugin;
 
-public class Main {
+import org.bukkit.plugin.java.JavaPlugin;
 
+public class Main extends JavaPlugin {
+public void onEnable(){
+	getLogger().info("Scoreboard plugin enabled!");
+}
+public void onDisable(){
+	getLogger().info("Scoreboard plugin disabled!");
+}
 }
